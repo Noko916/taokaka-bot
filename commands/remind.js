c6a5ac9c9eca41890ee17624351d40d2c9121112
Noko <Noko916@users.noreply.github.com>
2021-01-02 06:15:00 +0900

    Update remind.js

--- commands/remind.js
+++ commands/remind.js
@@ -3,28 +3,20 @@ const ms      = require("ms");
 const db      = require("quick.db");
 
 exports.run = (client, message, args) => {
-    
-    let remindtime  = args[0];
-    let reason      = args.slice(1).join(" ");
 
-    let reptime     = remindtime;
+    let remindtime = args[0];
+    let reason     = args.slice(1).join(" ");
 
-    reptime.replace(`d`, "日 ");
-    reptime.replace(`h`, "時間 ");
-    reptime.replace(`m`, "分 ");
-    //reptime.replace(`s`, "秒");
-    reptime.replace("秒", `s`);
-
-    if(!remindtime) return message.reply(":x: 時間を入力してください `例: .remind 10s aiueo`");
-    if(!reason)     return message.reply(":x: 内容を入力してください `例: .remind 10s aiueo`");
+    if (!remindtime) return message.reply(":x: 時間を入力してください `例: .remind 10s aiueo`");
+    if (!reason)     return message.reply(":x: 内容を入力してください `例: .remind 10s aiueo`");
 
     db.set(`remind.${message.author.id}`, Date.now() + ms(remindtime))
 
-    message.channel.send("OK! `"+ reptime + "` 後に `" + reason + "` をお知らせします");
+    message.channel.send("OK! `" + remindtime + "` 後に `" + reason + "` をお知らせします");
 
-    const interval = setInterval(function(){
+    const interval = setInterval(function () {
 
-        if(Date.now() > db.fetch(`remind.${message.author.id}`)){
+        if (Date.now() > db.fetch(`remind.${message.author.id}`)) {
             db.delete(`remind.${message.author.id}`);
             message.channel.send(`${message.author}\n**Remind:** ${reason}`)
                            .catch(e => console.log(e));
